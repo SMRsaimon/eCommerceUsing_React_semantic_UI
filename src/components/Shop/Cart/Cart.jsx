fc89385a92cb7551821e7f7767ec1ec8f3e599ca
SMRsaimon <smrsaimon960@gmail.com>
2021-06-18 07:49:41 -0700

    cart design responsive

--- src/components/Shop/Cart/Cart.jsx
+++ src/components/Shop/Cart/Cart.jsx
@@ -1,12 +1,38 @@
 import React from "react";
-import { Grid, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { Grid, Header, Image } from "semantic-ui-react";
 import "./Cart.css";
 
-export const Cart = ({products}) => {
+export const Cart = ({ products }) => {
+  console.log(products);
+
+  const { img, key, name, price, seller } = products;
+
+  const center = {
+    display: "flex",
+    justifyContent: "center",
+  };
   return (
     <>
-      <Grid.Column style={{display:"flex", justifyContent:"center"}} columns={4} mobile={16} tablet={8} computer={4}>
-        <Image src="/images/wireframe/media-paragraph.png" />
+      <Grid.Column style={center} columns={3} mobile={16} tablet={8} computer={5}>
+        <Grid>
+          <Grid.Column
+            style={center}
+            mobile={15}
+            tablet={8}
+            computer={8}
+          >
+            <Image src={img} />
+          </Grid.Column>
+          <Grid.Column
+            style={center}
+            mobile={15}
+            tablet={8}
+            computer={8}
+          >
+            <Header as={Link}>{name.slice(0, 20)}...</Header>
+          </Grid.Column>
+        </Grid>
       </Grid.Column>
     </>
   );
