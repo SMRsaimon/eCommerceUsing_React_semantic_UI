ce7a83b4f0e57bd4ef11f44184e3d967218df68a
SMRsaimon <smrsaimon960@gmail.com>
2021-06-18 05:28:27 -0700

    projects setup and reducer setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import { createContext, useReducer } from "react";
+import cartReducer, { initialState } from "./Reducer/reducers/cartReducer";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+export const globalContext = createContext({});
 function App() {
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <globalContext.Provider
+      value={{ state, dispatch }}
+    ></globalContext.Provider>
   );
 }
 
